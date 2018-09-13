1ae14c1f5322d56638d3305516fc1a9919dfc4fe
zhangheng <zhangheng_07@qq.com>
2018-09-13 18:18:52 +0800

    整数反转

---
+++ src/leetcode/AAAHreverseInteger.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+
+/**
+ * 
+ * https://leetcode-cn.com/problems/reverse-integer/description/
+ * @author zhangheng
+ *
+ */
+public class AAAHreverseInteger {
+	
+	public int reverse(int x) {
+		String str = String.valueOf(x);
+		StringBuffer sb = new StringBuffer();
+        if (x < 0) {
+        	str = str.substring(1, str.length());
+        	sb.append("-");
+        }
+        sb.append(new StringBuffer(str).reverse());
+        try {
+        	return Integer.valueOf(sb.toString());
+        } catch (NumberFormatException e) {
+        	
+        }
+        return 0;
+    } 
+	
+	public static void main(String[] args) {
+		AAAHreverseInteger a = new AAAHreverseInteger();
+		System.out.println(a.reverse(1534236469));
+	}
+	
+}
