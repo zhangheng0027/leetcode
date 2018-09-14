f29eaf6cb72002799dd8601d5666801a90860b3a
zhangheng <zhangheng_07@qq.com>
2018-09-14 10:49:02 +0800

    盛最多水的容器

---
+++ src/leetcode/AAAKcontainerWithMostWater.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+
+/**
+ * 
+ * https://leetcode-cn.com/problems/container-with-most-water/description/
+ * @author ZhangHeng
+ *
+ */
+public class AAAKcontainerWithMostWater {
+
+	public int maxArea(int[] height) {
+        
+		int s = 0;
+		for (int left = 0, right = height.length - 1; left < right;) {
+			int a = (right - left) * Math.min(height[left], height[right]);
+			if (a > s)
+				s = a;
+			if (height[left] < height[right])
+				left++;
+			else right--;
+		}
+		
+		return s;
+    }
+	
+	
+	public static void main(String[] args) {
+		int[] arr = {1,8,6,2,5,4,8,3,7};
+		AAAKcontainerWithMostWater a = new AAAKcontainerWithMostWater();
+		System.out.println(a.maxArea(arr));
+		
+	}
+}
