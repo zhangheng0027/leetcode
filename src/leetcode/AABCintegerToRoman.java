84918578b95bdb47f12a8b7c23d501125104777a
zhangheng <zhangheng_07@qq.com>
2018-09-14 17:36:45 +0800

    整数转罗马数字

---
+++ src/leetcode/AABCintegerToRoman.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/**
+ * 
+ * https://leetcode-cn.com/problems/integer-to-roman/description/
+ * @author ZhangHeng
+ *
+ */
+public class AABCintegerToRoman {
+	
+	private final static String[] roman = {"", "", "I", "V", "X", "L", "C", "D", "M"};
+	private final static int[]    radix = { 0,  0,  1,   5,   10,  50, 100, 500, 1000};
+	
+	public String intToRoman(int num) {
+		String[] roman = {"", "", "I", "V", "X", "L", "C", "D", "M"};
+		int[]    radix = { 0,  0,  1,   5,   10,  50, 100, 500, 1000};
+		StringBuffer sb = new StringBuffer();
+		for (int i = radix.length - 1; i >= 2 && num > 0; i--) {
+			if (num + radix[(i - 2) + i % 2] >= radix[i]) {
+				if (num >= radix[i]) {
+					sb.append(roman[i]);
+					num -= radix[i];
+					i++;
+				} else {
+					sb.append(roman[(i - 2) + i % 2]).append(roman[i]);
+					num = num + radix[(i - 2) + i % 2] - radix[i];
+				}
+				
+			}
+		}
+		return sb.toString();
+    }
+	
+	
+	/**
+	 */
+	public static void main(String[] args) {
+		AABCintegerToRoman a = new AABCintegerToRoman();
+		//for (int i = 40; i < 60; i++)
+		System.out.println(a.intToRoman(3));
+	}
+	
+}
