bf66f70b034222b7cca291a824d6295661dd6c57
zhangheng <zhangheng_07@qq.com>
2018-09-14 10:15:32 +0800

    回文整数

---
+++ src/leetcode/AAAJpalindromeNumber.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+/**
+ * 
+ * https://leetcode-cn.com/problems/palindrome-number/description/
+ * @author ZhangHeng
+ *
+ */
+public class AAAJpalindromeNumber {
+	public boolean isPalindrome(int x) {
+        
+		char[] c = String.valueOf(x).toCharArray();
+		int left = 0;
+		int right = c.length - 1;
+		
+		while (left <= right) {
+			if (c[left] != c[right])
+				return false;
+			left++;
+			right--;
+		}
+		return true;
+    }
+	
+	public static void main(String[] args) {
+		AAAJpalindromeNumber a = new AAAJpalindromeNumber();
+		System.out.println(a.isPalindrome(12321));
+	}
+}
