b70ce6ef5836c00de9cc4a20bcde0aeaa8466cce
zhangheng <1049725878@qq.com>
2019-03-11 19:03:02 +0800

    最长上升子序列

---
+++ src/main/java/com/leetcode/A300LengthOfLIS.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+/**
+ * Created by zh on 2019/3/11.
+ */
+public class A300LengthOfLIS {
+
+    public static void main(String[] args) {
+        int[] arg = {10,9,2,5,3,7,101,18};
+        System.out.println(lengthOfLIS(arg));
+
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        if (null == nums || nums.length == 0)
+            return 0;
+        final int len = nums.length;
+        int[] args = new int[len];
+        args[0] = 1;
+        for (int i = 1; i < len; i++) {
+            int max = 0;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && max < args[j])
+                    max = args[j];
+            }
+            args[i] = max + 1;
+        }
+        int max = 1;
+        for (int i : args)
+            if (max < i)
+                max = i;
+        return max;
+    }
+
+}
